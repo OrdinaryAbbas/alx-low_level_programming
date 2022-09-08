1b32fa16bb719a1b0c311ffe1a35163ae0784a6b
OrdinaryAbbas <sadiq.j.a62@gmail.com>
2022-09-08 10:51:34 -0700

    Third advancd task correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <uninstd.h>
 #include <stdlib.h>
 /**
  *main - Entry point
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora
+	write(2, "and that piece of art is useful\" - Dora
 						Korpar, 2015-10-19\n", 59);
 	return (1);
 }
