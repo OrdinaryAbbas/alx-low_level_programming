3c505a71c39b7adb3883a551903315115b7dd066
OrdinaryAbbas <sadiq.j.a62@gmail.com>
2022-09-09 10:06:05 -0700

    First if else commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time>
+
+/*
+ * main function is the entry point
+ * Contains a random number generator
+ * Contains if else statements to check the nature of number
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else
+	{
+		printf("%d is zero", n);
+	}
+
+	return (0);
