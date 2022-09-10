d884e020637eee66feb063eca9b404bdab577a67
OrdinaryAbbas <sadiq.j.a62@gmail.com>
2022-09-10 02:54:13 -0700

    Corrected 1 and 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*
+/**
  * main function is the entry point
  * Contains a random number generator
  * Contains if else statements to check the nature of number
+ * Return: Success always (0)
  */
 int main(void)
 {
@@ -16,15 +17,15 @@ int main(void)
 
 	if (n < 0)
 	{
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
 	else if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	}
 	else
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 
 	return (0);
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*
+/**
  * main is the entry point of the program
  * It creates a random number each time it runs
  * It contains selection statements
@@ -18,16 +18,16 @@ int main(void)
 
 	if (last_Digit > 5)
 	{
-		printf("The last digit of %d is %d and is greater than 5", n, last_Digit);
+		printf("The last digit of %d is %d and is greater than 5\n", n, last_Digit);
 	}
 	else if (!(last_Digit == 0) && (last_Digit < 6))
 	{
-		printf("The last digit of %d is %d and is less than 6 and not 0"
-								, n, last_Digit);
+		printf("The last digit of %d is %d and is less than 6
+					and not 0\n", n, last_Digit);
 	}
 	else
 	{
-		printf("The last digit of %d is %d and is 0", n, last_Digit);
+		printf("The last digit of %d is %d and is 0\n", n, last_Digit);
 	}
 
 	return (0);
