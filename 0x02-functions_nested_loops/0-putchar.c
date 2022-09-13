84d97f8298dfa4829863de724f94b02eee1f3246
OrdinaryAbbas <sadiq.j.a62@gmail.com>
2022-09-13 14:28:41 -0700

    Task 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main-is the entry point
+* it prints the word _putchar to the screen
+* Return: Success(0)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
